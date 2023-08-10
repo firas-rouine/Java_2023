8709091fc1ba394b3f05048c46388edb2e6e9b9e
firas-rouine <rouine.firas@gmail.com>
2023-08-10 15:47:36 +0100

    RenderingBooks commit

---
+++ Week2/Day 2/Practice/RenderingBooks/src/main/java/com/firas/RenderingBooks/controllers/BookController.java
@@ -0,0 +1,34 @@
+package com.firas.RenderingBooks.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.firas.RenderingBooks.Services.BookService;
+import com.firas.RenderingBooks.models.Book;
+
+@Controller
+public class BookController {
+	
+	// import the bookService
+	private final BookService bookService;
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+
+	
+
+	@RequestMapping("/books/{id}")
+	public String showOne(@PathVariable("id") Long id,
+					Model model) {
+		
+		// fetch the book from DB
+		Book thisBook = bookService.findBook(id);
+		
+		// pass the book the jsp page
+		model.addAttribute("book",thisBook);
+		
+		return "show.jsp";
+	}
+}
